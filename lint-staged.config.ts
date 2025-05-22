eabf05f79276fee72a94db2bf62c760becd1a297
Stefan Karlsson <iam@stefan-karlsson.se>
2025-05-22 13:10:47 +0200

    chore: update linting commands in configuration files
    - Changed lint-staged configuration to use `pnpm lint:fix` instead of `eslint --fix`.
    - Updated GitHub Actions workflow to run `pnpm lint` for code linting.

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -2,7 +2,7 @@ export default {
     "*.{js,mjs,cjs,jsx,ts,mts,cts,tsx,md,html,css}":{ 
         title: 'Lint and format',
         task: async (stagedFiles: string[]) =>  [
-            `eslint --fix`, 
+            `pnpm lint:fix`, 
             `prettier --ignore-unknown --write ${stagedFiles.join(' ')}`
         ]
     }
