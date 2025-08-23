0469bfde56e3cb7bdd4a3f9999a0f87d67bef785
Stefan Karlsson <iam@stefan-karlsson.se>
2025-08-23 15:14:59 +0200

    refactor: core structure

---
+++ packages/server/exceptions/tests/exceptions.test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from "node:test";
+import assert from "node:assert";
+
+describe('@aviene/exceptions', () => {
+  it('scaffold runs', () => {
+    assert.equal(true, true);
+  });
+});
--- packages/shared/utils.types/eslint.config.js
+++ packages/shared/utils.types/eslint.config.js
@@ -1,6 +1,5 @@
 import base from '@aviene/eslint.config/base';
 
 export default [
-  ...base,
-  // package-specific tweaks here
+  ...base
 ];
